8ee05ea12eed82ae8368cbc27744e219c1b7c7fd
Alain Papazoglou <alain@eliga.fr>
2013-12-11 13:34:38 +0100

    First commit with code

---
+++ module/lib/some_other_code.js
@@ -0,0 +1,12 @@
+if (typeof ObjetB === "undefined") {
+	console.log("ObjetB", "undefined");
+} else {
+	console.log("ObjetB", "defined:", ObjetB);
+}
+
+if (typeof ObjetA === "undefined") {
+	console.log("ObjetA", "undefined");
+} else {
+	console.log("ObjetA", "defined:", ObjetA);
+}
+
